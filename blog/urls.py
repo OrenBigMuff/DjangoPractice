00c85868f079cc489f7b4255059872915b1a5713
oinarhythm <oinarhythm0508@gmail.com>
2022-03-27 20:29:59 +0900

    develop blog post func

--- blog/urls.py
+++ blog/urls.py
@@ -1,3 +1,11 @@
 from django.urls import path
-from blog.views import PostList
-urlpatterns = [path("", PostList.as_view(), name="post_list"), ]
+from blog.views import PostList, PostDetail
+
+urlpatterns = [
+    path("", PostList.as_view(), name="post_list"),
+    path(
+        "Post/<int:pk>/",
+        PostDetail.as_view(),
+        name="post_detail",
+    ),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,14 @@
-from pdb import post_mortem
-from django.views.generic import ListView
-
+from django.views.generic import ListView, DetailView
 from blog.models import Post
+
 
 class PostList(ListView):
     model = Post
     template_name = "blog/index.html"
     context_object_name = "posts"
+
+
+# DetailViewを継承したクラスで記述する中身はListViewと変わらない
+class PostDetail(DetailView):
+    model = Post
+    context_object_name = "post"
